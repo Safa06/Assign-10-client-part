a5df9a143beda9874b1e033a3c8faff6e248905a
Safa06 <sukonna.safa@gmail.com>
2025-11-09 21:25:12 +0600

    commit-2

---
+++ .history/src/layout/MainLayout_20251109212500.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <div className="w-full mx-auto">
+        <div className="shadow-md px-4">
+          <Navbar />
+        </div>
+
+        <div className="mt-4">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default MainLayout;
